e0e7cde05e965a212f8b1bcab8ce8fe7e0f51803
Aspectious <griffin.siebert14@outlook.com>
2024-10-07 14:04:56 -0400

    Updated Server FIles

--- clientServer/Main.java
+++ clientServer/Main.java
@@ -1,11 +1,11 @@
-package net.eastern.FlyAway
+package net.eastern.FlyAway;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-import cpm.sun.net.httpserver.*;
-Public class  {
+import com.sun.net.httpserver.*;
+public class Main {
   public static void main(String[]args) throws Exception {
     HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
     server.createContext("/", new MyHandler());
@@ -14,7 +14,7 @@ Public class  {
   }
 
   static class MyHandler implements HttpHandler {
-    @override
+    @Override
     public void handle(HttpExchange t) throws IOException {
       String response = "This is the response";
       t.sendResponseHeaders(200,response.length());
