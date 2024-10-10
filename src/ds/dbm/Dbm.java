d87c1a5c624731ba612dcdd1592fac03d833e23e
Aspectious <griffin.siebert14@outlook.com>
2024-10-09 20:29:08 -0400

    Added more classes and packages, added dbm

---
+++ src/ds/dbm/Dbm.java
@@ -0,0 +1,30 @@
+package net.eastern.FlyAway.dbm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbm {
+    private Connection dbconn;
+    public Dbm() {
+        // For Testing
+        attemptConnection("jdbc:mysql://localhost:3306/records", "java",System.getenv("FLYAWAY_DBM_PWD"));
+    }
+
+    /*
+        For Production, the url will be jdbc:mysql://localhost:3306/records
+        Username java, password is a hashed secret in an Environmental Variable
+        Feel Free to use any other url or user/pass combo for dev
+        TODO: NEVER PUT ANY RAW PASSWORDS IN AND COMMIT, IT IS MOSTLY IRREVERSIBLE
+     */
+    public void attemptConnection(String url, String user, String pass) {
+        System.out.println("Attempting to connect to " + url);
+        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Successfully connected to " + url);
+            this.dbconn = connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException("Unable to connect to " + url, e);
+        }
+
+    }
+}
