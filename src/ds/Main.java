b1c7257a97232a50bece522630fe7b20d9d6f765
scushi <galaxydeath7281@gmail.com>
2024-10-10 14:19:24 -0400

    we fucked up batman

--- src/ds/Main.java
+++ src/ds/Main.java
@@ -2,12 +2,14 @@ package net.eastern.FlyAway;
 
 
 
+import net.eastern.FlyAway.CLI.Input;
 import net.eastern.FlyAway.dbm.Dbm;
 import net.eastern.FlyAway.web.Server;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Server webserver = new Server(8000);
+        Input input = new Input();
         //Dbm databasemanager = new Dbm();
     }
 }
