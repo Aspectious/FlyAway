42df4473d7ee118ea32995146c6a8bd959113c6f
Griffin D. Siebert <gdsiebert@outlook.com>
2024-10-23 17:00:05 -0400

    its aliveeeeeeeee

--- ep/cmake-build-release/cmake_install.cmake
+++ ep/cmake-build-release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/scushi/CLionProjects/FlyAway/ep
+# Install script for directory: /home/aspectious/dev/flyaway/ep
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/scushi/CLionProjects/FlyAway/ep/cmake-build-release/_deps/cpr-build/cmake_install.cmake")
+  include("/home/aspectious/dev/flyaway/ep/cmake-build-release/_deps/cpr-build/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -55,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/scushi/CLionProjects/FlyAway/ep/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/aspectious/dev/flyaway/ep/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- ep/src/main.cpp
+++ ep/src/main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 int main() {
-    auto response = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"});
+    cpr::SslOptions sslOpts = cpr::Ssl(cpr::ssl::VerifyHost{false}, cpr::ssl::VerifyPeer{false},cpr::ssl::VerifyStatus{false});
+    auto response = cpr::Get(cpr::Url{"https://localhost:8000/validate?id=845435"}, sslOpts);
     std::cout << response.text << std::endl;
     return 0;
 }
