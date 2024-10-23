9085b3dfb10eda61693c65e48ae83ba6227379cc
SushiBirb <galaxydeath7281@gmail.com>
2024-10-23 17:54:12 -0400

    started c work, maybe this will increase how well project may be imported from one instance to another

---
+++ ep/cmake-build-release/CMakeFiles/ep.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ep.dir/src/main.cpp.o"
+  "CMakeFiles/ep.dir/src/main.cpp.o.d"
+  "ep"
+  "ep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
